99f128047f82cbb3f06f8754eeb1971469f58671
GabrielF-S <gabriel.fsantos97@outlook.com>
2026-07-25 19:29:36 -0300

    :sparkles: feat: Criado metodo para busca de Employees

--- findaseat/src/main/java/com/gabsdev/findaseat/repository/UserRepository.java
+++ findaseat/src/main/java/com/gabsdev/findaseat/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.gabsdev.findaseat.repository;
 
+import com.gabsdev.findaseat.model.entity.Employee;
 import com.gabsdev.findaseat.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmployees_Id(Long id);
 }
