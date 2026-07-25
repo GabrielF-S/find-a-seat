e1dbb41f0e3e05e6bddf70ec1a55603ce373823c
GabrielF-S <gabriel.fsantos97@outlook.com>
2026-07-25 19:31:34 -0300

    :card_file_box: raw: Adicionado coluna a tabela tb_reservation

--- findaseat/src/main/java/com/gabsdev/findaseat/model/enums/ReservationStatus.java
+++ findaseat/src/main/java/com/gabsdev/findaseat/model/enums/ReservationStatus.java
@@ -3,7 +3,8 @@ package com.gabsdev.findaseat.model.enums;
 public enum ReservationStatus {
     PENDING(1L, "pending"),
     CONFIRMED(2L, "confirmed"),
-    CANCELLED(3L, "cancelled");
+    CANCELLED(3L, "cancelled"),
+    NOT_CONFIRMED(4L, "not_confirmed");
 
     ReservationStatus(Long id, String statusName) {
     }
