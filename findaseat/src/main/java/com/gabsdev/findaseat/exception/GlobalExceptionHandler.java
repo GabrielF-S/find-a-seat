fa1ac7c8eb56b625ef534a94840409ce40be7c69
GabrielF-S <gabriel.fsantos97@outlook.com>
2026-08-12 19:11:31 -0300

    :sparkles: feat: Adicionado  ExceptionHandler das demais exception

--- findaseat/src/main/java/com/gabsdev/findaseat/exception/GlobalExceptionHandler.java
+++ findaseat/src/main/java/com/gabsdev/findaseat/exception/GlobalExceptionHandler.java
@@ -17,4 +17,70 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ProblemDetail> handle(BusinessNotFoundException exception){
         return ResponseEntity.of(exception.toProblemDetail()).build();
     }
+    @ExceptionHandler
+    public ResponseEntity<ProblemDetail> handle(BusinessExistsException exception){
+        return ResponseEntity.of(exception.toProblemDetail()).build();
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ProblemDetail> handle(ConflictReservationException exception){
+        return ResponseEntity.of(exception.toProblemDetail()).build();
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ProblemDetail> handle(EmployeeNotFoundException exception){
+        return ResponseEntity.of(exception.toProblemDetail()).build();
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ProblemDetail> handle(FloorAlredyExistException exception){
+        return ResponseEntity.of(exception.toProblemDetail()).build();
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ProblemDetail> handle(FloorNoFoundException exception){
+        return ResponseEntity.of(exception.toProblemDetail()).build();
+    }
+    @ExceptionHandler
+    public ResponseEntity<ProblemDetail> handle(NumberOfSeatsException exception){
+        return ResponseEntity.of(exception.toProblemDetail()).build();
+    }
+    @ExceptionHandler
+    public ResponseEntity<ProblemDetail> handle(ReservationConflictException exception){
+        return ResponseEntity.of(exception.toProblemDetail()).build();
+    }
+    @ExceptionHandler
+    public ResponseEntity<ProblemDetail> handle(ReservationDesativatedException exception){
+        return ResponseEntity.of(exception.toProblemDetail()).build();
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ProblemDetail> handle(ReservationNotFoundException exception){
+        return ResponseEntity.of(exception.toProblemDetail()).build();
+    }
+    @ExceptionHandler
+    public ResponseEntity<ProblemDetail> handle(ReservationPeriodDayException exception){
+        return ResponseEntity.of(exception.toProblemDetail()).build();
+    }
+    @ExceptionHandler
+    public ResponseEntity<ProblemDetail> handle(SeatAlredyExistException exception){
+        return ResponseEntity.of(exception.toProblemDetail()).build();
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ProblemDetail> handle(SeatNotFoundException exception){
+        return ResponseEntity.of(exception.toProblemDetail()).build();
+    }
+    @ExceptionHandler
+    public ResponseEntity<ProblemDetail> handle(UserNotFoundException exception){
+        return ResponseEntity.of(exception.toProblemDetail()).build();
+    }
+    @ExceptionHandler
+    public ResponseEntity<ProblemDetail> handle(WaitlistException exception){
+        return ResponseEntity.of(exception.toProblemDetail()).build();
+    }
+    @ExceptionHandler
+    public ResponseEntity<ProblemDetail> handle(WaitlistNotFoundException exception){
+        return ResponseEntity.of(exception.toProblemDetail()).build();
+    }
 }
