d942eff35b3019c3fdf998c58b70970b25bda00a
GabrielF-S <gabriel.fsantos97@outlook.com>
2026-09-14 18:53:49 -0300

    :sparkles: feat: Criado controller para Autenticação de usuario

--- findaseat/src/main/java/com/gabsdev/findaseat/controller/AuthController.java
+++ findaseat/src/main/java/com/gabsdev/findaseat/controller/AuthController.java
@@ -1,6 +1,26 @@
 package com.gabsdev.findaseat.controller;
 
+import com.gabsdev.findaseat.dto.request.LoginRequest;
+import com.gabsdev.findaseat.dto.request.RegisterUserRequest;
+import com.gabsdev.findaseat.dto.response.LoginResponse;
+import com.gabsdev.findaseat.dto.response.RegisterUserResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public interface AuthController {
-//    @PostMapping("/login")
-//    ResponseEntity<LoginResponse> login(@RequestBody LoginRequest login);
+
+    @PostMapping("/login")
+    ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request);
+
+    @PostMapping("/register")
+    ResponseEntity<RegisterUserResponse> registerUserResponseResponseEntity(@Valid @RequestBody RegisterUserRequest request);
+
+    @PostMapping("adm/register")
+    ResponseEntity<RegisterUserResponse> registerAdminUser(@Valid @RequestBody RegisterUserRequest request);
+
+    @PostMapping("super/register")
+    ResponseEntity<RegisterUserResponse> registerSuperUser(@Valid @RequestBody RegisterUserRequest request);
+
 }
--- findaseat/src/main/java/com/gabsdev/findaseat/controller/impl/AuthControllerImpl.java
+++ findaseat/src/main/java/com/gabsdev/findaseat/controller/impl/AuthControllerImpl.java
@@ -1,9 +1,18 @@
 package com.gabsdev.findaseat.controller.impl;
 
 import com.gabsdev.findaseat.controller.AuthController;
+import com.gabsdev.findaseat.dto.request.LoginRequest;
+import com.gabsdev.findaseat.dto.request.RegisterUserRequest;
+import com.gabsdev.findaseat.dto.response.LoginResponse;
+import com.gabsdev.findaseat.dto.response.RegisterUserResponse;
 import com.gabsdev.findaseat.service.AuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("auth")
+@RestController
 public class AuthControllerImpl implements AuthController {
     private  final AuthService service;
 
@@ -11,9 +20,25 @@ public class AuthControllerImpl implements AuthController {
         this.service = service;
     }
 
-//    @Override
-//    public ResponseEntity<LoginResponse> login(LoginRequest login) {
-//
-//        return ResponseEntity.ok(service.login(login));
-//    }
+    @Override
+    public ResponseEntity<LoginResponse> login(LoginRequest request) {
+        return ResponseEntity.ok(service.login(request));
+    }
+
+    @Override
+    public ResponseEntity<RegisterUserResponse> registerUserResponseResponseEntity(RegisterUserRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.createComumUser(request));
+    }
+
+    @Override
+    public ResponseEntity<RegisterUserResponse> registerAdminUser(RegisterUserRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.createAdminUser(request));
+    }
+
+    @Override
+    public ResponseEntity<RegisterUserResponse> registerSuperUser(RegisterUserRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.createSuperUser(request));
+    }
+
+
 }
