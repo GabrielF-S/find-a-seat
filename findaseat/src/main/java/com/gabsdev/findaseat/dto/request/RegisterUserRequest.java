84e5328e9d58ef224340f6a26348afbdd320c709
GabrielF-S <gabriel.fsantos97@outlook.com>
2026-09-14 18:54:46 -0300

    :sparkles: feat: adicionado atributos para dtos de regristro e e login

--- findaseat/src/main/java/com/gabsdev/findaseat/dto/request/RegisterUserRequest.java
+++ findaseat/src/main/java/com/gabsdev/findaseat/dto/request/RegisterUserRequest.java
@@ -2,5 +2,5 @@ package com.gabsdev.findaseat.dto.request;
 
 import java.util.List;
 
-public record RegisterUserRequest(String email, String password, List<String> roles, Long employeeID) {
+public record RegisterUserRequest(String email, String password, Long employeeID) {
 }
---
+++ findaseat/src/main/java/com/gabsdev/findaseat/dto/response/LoginResponse.java
@@ -0,0 +1,4 @@
+package com.gabsdev.findaseat.dto.response;
+
+public record LoginResponse(String token) {
+}
